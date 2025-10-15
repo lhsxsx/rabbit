f18298bd9da6df57364e186a9a240524a6e7a51a
志方 侯 <1440713747@qq.com>
2025-10-15 16:04:00 +0800

    home页面左侧导航栏

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,19 @@
+<script lang="js" setup>
+import HomeBanner from './components/HomeBanner.vue'
+import HomeCategory from './components/HomeCategory.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomePruduct from './components/HomePruduct.vue'
+</script>
 <template>
-  <div>
-    
+
+  <div class="container">
+
+    <HomeCategory />
+    <HomeBanner />
+
   </div>
+  <home-new />
+  <home-hot />
+  <home-Pruduct />
 </template>
