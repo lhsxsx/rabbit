dd4199866b90dadbc3374838a46a8fb4ea532250
志方 侯 <1440713747@qq.com>
2025-10-23 20:14:50 +0800

    滑块移动放大

--- src/components/index.vue
+++ src/components/index.vue
@@ -21,7 +21,12 @@ const target=ref(null)
 const { elementX,elementY,isOutside }=useMouseInElement(target)
 const top=ref(0)
 const left=ref(0)
+const positionX=ref(0)
+const positionY=ref(0)
 watch([elementX,elementY],()=>{
+  if(isOutside.value){
+    return
+  }
   if(elementX.value>100 && elementX.value<300){
     left.value=elementX.value-100
   }
@@ -32,7 +37,10 @@ watch([elementX,elementY],()=>{
   if(elementX<100){left.value=0}
   if(elementY>300){top.value=200}
   if(elementY<100){top.value=0}
+  positionX.value= -2*left.value,
+  positionY.value= -2*top.value
 }
+
 
 )
 </script>
@@ -43,7 +51,7 @@ watch([elementX,elementY],()=>{
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer"  v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -54,11 +62,11 @@ watch([elementX,elementY],()=>{
     <!-- 放大镜大图 -->
     <div class="large" :style="[
       {
-        backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundImage: `url(${imageList[activeIndex]})`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
