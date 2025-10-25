cbcc81fe841cc062485c4659ad3d7032f305cbf3
志方 侯 <1440713747@qq.com>
2025-10-25 17:05:36 +0800

    退出登录

--- src/views/Layout/components/LayoutNav.vue
+++ src/views/Layout/components/LayoutNav.vue
@@ -1,8 +1,14 @@
 <script setup>
 
 import { useUserStore } from '@/stores/user';
-const userstore=useUserStore()
+import { useRouter } from 'vue-router';
 
+const userstore=useUserStore()
+const router=useRouter()
+const confirm=()=>{
+  userstore.clearUserInfo()
+  router.push('/login')
+}
 </script>
 
 <template>
@@ -13,7 +19,7 @@ const userstore=useUserStore()
         <template v-if="userstore.userInfo.token">
           <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{ userstore.userInfo.account }}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
                 <a href="javascript:;">退出登录</a>
               </template>
