b3a0961ca245fbe10528520433096f170818f478
志方 侯 <1440713747@qq.com>
2025-10-25 17:22:29 +0800

    token401

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 import 'element-plus/es/components/message/style/css'
 import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/stores/user'
+import router from '@/router'
+
 
 // 创建axios实例
 const httpInstance = axios.create({
@@ -21,10 +23,15 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应拦截器
 httpInstance.interceptors.response.use(res => res.data, e => {
+  const userStore=useUserStore()
   ElMessage({
     type:'warning',
     message:e.response.data.message
   })
+  if(e.response.status===401){
+    userStore.clearUserInfo()
+    router.push('/login')
+  }
   return Promise.reject(e)
 })
 
