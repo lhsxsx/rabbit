1ec764436f7f5e78964579e0c897c219b03296a6
志方 侯 <1440713747@qq.com>
2025-10-25 11:54:21 +0800

    统一检验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -30,6 +30,16 @@ const rules={
     }
   ]
 }
+
+const formref=ref(null)
+const onSubmit=()=>{
+  formref.value.validate((valid)=>{
+    if(valid){
+      console.log('验证通过') 
+    }
+    }
+  )
+}
 </script>
 
 <template>
@@ -53,7 +63,7 @@ const rules={
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+            <el-form ref="formref" :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="form.account" />
               </el-form-item>
@@ -65,7 +75,7 @@ const rules={
                   我已同意隐私条款和服务条款
                 </el-checkbox >
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="onSubmit" >点击登录</el-button>
             </el-form>
           </div>
         </div>
