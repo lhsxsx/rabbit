c9695305e0b7de5386b5434fd84960b2506f90e7
志方 侯 <1440713747@qq.com>
2025-11-01 19:03:56 +0800

    列表切换

--- src/views/Member/components/UserOrder.vue
+++ src/views/Member/components/UserOrder.vue
@@ -24,11 +24,17 @@ const getOrderList=async()=>{
   orderList.value=res.result.items
 }
 onMounted(()=>getOrderList())
+
+const tabChange=(type)=>{
+  params.value.orderState=type
+  getOrderList()
+
+}
 </script>
 
 <template>
   <div class="order-container">
-    <el-tabs>
+    <el-tabs @tab-change="tabChange">
       <!-- tab切换 -->
       <el-tab-pane v-for="item in tabTypes" :key="item.name" :label="item.label" />
 
